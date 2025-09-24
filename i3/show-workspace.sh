2d906c64170d540eda9847f770772b31dbeebc0d
Sarkar069 <ishuthakur186@gmail.com>
2025-09-24 18:50:18 +0530

    workspace.sh update

--- i3/show-workspace.sh
+++ i3/show-workspace.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Get current workspace name
-CURRENT=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name')
+# Get list of workspaces, highlight the focused one
+ws=$(i3-msg -t get_workspaces | jq -r '
+    map(if .focused
+        then "<b><span foreground=\"#88c0d0\">[" + (.name|tostring) + "]</span></b>"
+        else .name end)
+    | join("\n")')
 
-# Send notification
-notify-send "🧭 Workspace" "You are on: $CURRENT"
-
+# Show notification with markup
+notify-send -t 3000 -u low "Workspaces" "$ws" \
+  -h string:x-canonical-private-synchronous:workspaces
